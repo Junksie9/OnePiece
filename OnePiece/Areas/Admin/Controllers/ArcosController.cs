6edfdb54d1c582943aa48f79d4f90ca4cefd2803
Junksie9 <89873667+Junksie9@users.noreply.github.com>
2021-12-07 11:06:49 -0600

    ArcosController Completo

--- OnePiece/Areas/Admin/Controllers/ArcosController.cs
+++ OnePiece/Areas/Admin/Controllers/ArcosController.cs
@@ -52,14 +52,81 @@ namespace OnePiece.Areas.Admin.Controllers
             return View(a);
         }
 
-        public IActionResult Editar()
+        public IActionResult Editar(int id)
         {
-            return View();
+            var arco = Context.Arcos.FirstOrDefault(x => x.IdArco == id);
+            if (arco==null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(arco);
+        }
+        [HttpPost]
+        public IActionResult Editar(Arco a)
+        {
+            var arc = Context.Arcos.FirstOrDefault(x => x.IdArco == a.IdArco);
+            if (arc == null)
+            {
+                ModelState.AddModelError("", "El arco ya no existe o se elimino");
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(a.NombreArco))
+                {
+                    ModelState.AddModelError("", "Agregue Nombre del arco");
+                }
+                else if (string.IsNullOrWhiteSpace(a.Descripcion))
+                {
+                    ModelState.AddModelError("", "Agregue una Descripcion");
+                }
+                else if (a.NumArco == 0)
+                {
+                    ModelState.AddModelError("", "El numero de arco no puede ir en 0 ");
+                }
+                else if (Context.Arcos.Any(x => x.NombreArco == a.NombreArco && x.IdArco!=a.IdArco))
+                {
+                    ModelState.AddModelError("", "Ya existe un arco con ese nombre");
+                }
+                else
+                {
+                    arc.NombreArco = a.NombreArco;
+                    arc.Descripcion = a.Descripcion;
+                    arc.NumArco = a.NumArco;
+                    Context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(a);
         }
 
-        public IActionResult Eliminar()
+        public IActionResult Eliminar(int id)
         {
-            return View();
+            var arc = Context.Arcos.FirstOrDefault(x => x.IdArco == id);
+            if (arc==null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(arc);
+        }
+        [HttpPost]
+        public IActionResult Eliminar(Arco a)
+        {
+            var arc = Context.Arcos.FirstOrDefault(x => x.IdArco == a.IdArco);
+            if (arc == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var c in arc.Capitulos)
+                {
+                    Context.Remove(c);
+                }
+                Context.Remove(arc);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(a);
         }
     }
 }
