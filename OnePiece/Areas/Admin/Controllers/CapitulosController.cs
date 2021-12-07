35983e174b89619e2f398215d4e063bf5d914205
Junksie9 <89873667+Junksie9@users.noreply.github.com>
2021-12-06 20:08:33 -0600

    Admin

---
+++ OnePiece/Areas/Admin/Controllers/CapitulosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnePiece.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CapitulosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnePiece/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnePiece.Areas.Admin.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        [Route("admin")]
+        [Route("admin/Home")]
+        [Route("admin/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
